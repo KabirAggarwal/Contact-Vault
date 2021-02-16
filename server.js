4ab878b4f110ab396fd2c714da45767fe69ecaac
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2021-02-16 20:51:01 +0530

    commits

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.json({ msg: 'Welcome to Contacts API' }));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server on 5000`));
